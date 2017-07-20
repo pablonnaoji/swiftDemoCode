373e4434957b03db29b0e4a424ded9e2bd9ad7dd
pablonnaoji <pnnaoji@alumni.berklee.edu>
2017-07-20 12:04:26 -0700

    sending generic boilerplate code

---
+++ AskAGuru/SetListModelController.swift
@@ -0,0 +1,9 @@
+//
+//  SetListModelController.swift
+//  AskAGuru
+//
+//  Created by Paul Nnaoji on 7/13/17.
+//  Copyright © 2017 SGG. All rights reserved.
+//
+
+import Foundation
---
+++ AskAGuru/Stateful.swift
@@ -0,0 +1,9 @@
+//
+//  Stateful.swift
+//  AskAGuru
+//
+//  Created by Paul Nnaoji on 7/19/17.
+//  Copyright © 2017 SGG. All rights reserved.
+//
+
+import Foundation
